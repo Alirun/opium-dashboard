9055e5139537df98aae0e0b830af43595cc52f95
Timur <bobetbat@Regiv-iMac.local>
2020-05-23 21:16:35 +0200

    routing

---
+++ frontend/src/Views/Core/index.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import NavRoutes from '../../Components/NavRoutes'
+
+import './styles.scss'
+
+type Props = {}
+
+type State = {
+  loading: boolean
+}
+
+class Overview extends Component<Props, State> {
+  constructor (props: Props) {
+    super(props)
+    this.state = {
+      loading: true
+    }
+  }
+  
+  componentDidMount () {
+    this.setState({
+      loading: false
+    })
+  }
+
+  render () {
+    return (
+      <div className='overviewWrapper'>
+        <NavRoutes activeTab='/core' additionalClassName='overviewRoutes' />
+        <div className='overviewBody'></div>
+      </div>
+    )
+  }
+}
+
+export default Overview
